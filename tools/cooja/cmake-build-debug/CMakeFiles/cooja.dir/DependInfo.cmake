3bbe4900bd7dd87401c07d86eee27ed05854ffa3
Satya Prakash <mathayus1729@gmail.com>
2018-10-02 13:06:27 +0530

    First Commit

---
+++ tools/cooja/cmake-build-debug/CMakeFiles/cooja.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/media/mathayus/Windows/contiki/tools/cooja/build/test_template.c" "/media/mathayus/Windows/contiki/tools/cooja/cmake-build-debug/CMakeFiles/cooja.dir/build/test_template.c.o"
+  "/media/mathayus/Windows/contiki/tools/cooja/config/test_template.c" "/media/mathayus/Windows/contiki/tools/cooja/cmake-build-debug/CMakeFiles/cooja.dir/config/test_template.c.o"
+  "/media/mathayus/Windows/contiki/tools/cooja/examples/project_new_interface/dummy_intf.c" "/media/mathayus/Windows/contiki/tools/cooja/cmake-build-debug/CMakeFiles/cooja.dir/examples/project_new_interface/dummy_intf.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../examples/project_new_interface"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tools/cooja/cmake-build-debug/CMakeFiles/cooja.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cooja.dir/build/test_template.c.o"
+  "CMakeFiles/cooja.dir/config/test_template.c.o"
+  "CMakeFiles/cooja.dir/examples/project_new_interface/dummy_intf.c.o"
+  "cooja.pdb"
+  "cooja"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cooja.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
